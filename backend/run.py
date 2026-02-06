9a2e1691f1a5f8137b57f9ba91ff8dced9a7b11b
iamshaury <officialshaury@gmail.com>
2026-02-06 21:27:41 +0530

    port

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,4 @@ from application import app
 # app is already created in application.py
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
